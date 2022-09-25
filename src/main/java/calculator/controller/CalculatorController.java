94c780c6195e65e130582d494ab4be4c30360a6b
WildVash <indigo.kun@gmail.com>
2022-09-25 15:24:08 +0300

    Add controler and template

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,8 +1,24 @@
 package calculator.controller;
 
+import calculator.service.interfaces.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
+    private final CalculatorService calculatorService;
 
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @RequestMapping("/calculate")
+    @ResponseBody
+    public String calculate(@RequestBody String input) {
+        return calculatorService.calculate(input);
+    }
 }
